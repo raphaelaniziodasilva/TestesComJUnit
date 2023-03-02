0f7c714474e472525004d9945ee150f287d116c1
Raphael Anizio <raphaelanizio01@gmail.com>
2023-03-02 20:31:23 -0300

    Testes automatizados com JUnit

--- src/r43.java
+++
@@ -1,8 +0,0 @@
-public class r43 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-    }
-}
